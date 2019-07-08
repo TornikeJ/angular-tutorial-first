b813067e6bf2e80d9ebc98b35a1cd835382c1a75
Tornike Janashia <t.janashia11@gmail.com>
2019-07-08 20:23:30 +0400

    feat(10-form-validation-advanced-cross-field-validation): Finish task

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
 import {
   FormBuilder,
-  Validators
+  Validators,
+  FormControl,
+  FormGroup
 } from '@angular/forms';
 import { WishlistService } from '../wishlist.service';
 
@@ -22,15 +24,46 @@ export class CartComponent implements OnInit {
     private wishlistService: WishlistService) {
     this.items = this.cartService.getItems();
     this.checkoutForm = this.formBuilder.group({
-      name: ['', [Validators.minLength(2)]],
+      name: ['', [this.forbiddenName(), Validators.minLength(4)]],
       address: this.formBuilder.group({
         street: '',
         city: '',
         state: '',
         zip: ''
-      })
+      }, {
+          validators: this.crossValidation
+        })
     });
   }
+
+
+  static isZipValid(zip) {
+    return zip.length < 3;
+  }
+  static isCityValid(city) {
+    return city && city[0].toLowerCase() === 'a';
+  }
+  crossValidation(formGroup) {
+    const zip = formGroup.get('zip').value;
+    const zipStatus = CartComponent.isZipValid(zip);
+
+    const city = formGroup.get('city').value;
+    const cityStatus = CartComponent.isCityValid(city);
+
+    const validationResult = {
+      zipStatus,
+      cityStatus
+    };
+
+    return validationResult.zipStatus && validationResult.cityStatus ? null : validationResult;
+  }
+
+  forbiddenName() {
+    return (formControl) => {
+      return formControl.value === 'Oliver' ? { forbiddenName: { invalid: true } } : null;
+    }
+  }
+
   onSubmit(customerData) {
     console.warn('Your order has been submitted', customerData);
     this.items = this.cartService.clearCart();
@@ -43,6 +76,24 @@ export class CartComponent implements OnInit {
   }
   ngOnInit() {
 
+  }
+  get name() {
+    return this.checkoutForm.get('name') as FormControl;
+  }
+  get address() {
+    return this.checkoutForm.get('address') as FormGroup;
+  }
+  get city() {
+    return this.checkoutForm.get('address').get('city') as FormControl;
+  }
+  get zip() {
+    return this.checkoutForm.get('address').get('zip') as FormControl;
+  }
+  get state() {
+    return this.checkoutForm.get('address').get('state') as FormControl;
+  }
+  get street() {
+    return this.checkoutForm.get('address').get('street') as FormControl;
   }
 
   removeItem(item) {
