47430fabc3afaa3456bc5da0a7ca85c671c24a3a
Tornike Janashia <t.janashia11@gmail.com>
2019-07-10 12:20:24 +0400

    feat(Rxjs-task-1): Finish practice

--- src/app/exchange/exchange.component.ts
+++ src/app/exchange/exchange.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { data } from '../currencies';
-import { FormBuilder } from '@angular/forms';
-import { ExchangeService } from '../exchange.service';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-exchange',
@@ -9,37 +10,77 @@ import { ExchangeService } from '../exchange.service';
   styleUrls: ['./exchange.component.scss']
 })
 export class ExchangeComponent implements OnInit {
-
+  currencies;
+  Observer;
   currencyBase = [];
   exchangeForm;
   baseCurrency;
   targetCurrency;
-  updatedCurrency;
+  exchangeRate;
+  updatedValue;
   constructor(
-    private formBuilder: FormBuilder,
-    private exchangeService: ExchangeService) {
+    private httpClient: HttpClient,
+    private formBuilder: FormBuilder) {
     this.exchangeForm = this.formBuilder.group({
-      baseCurrency: [],
-      targetCurrency: [],
+      base: [],
+      target: [],
     });
     this.currencyBase = data;
   }
 
+  get base() {
+    return this.exchangeForm.get('base') as FormControl;
+  }
+  get target() {
+    return this.exchangeForm.get('target') as FormControl;
+  }
   ngOnInit() {
+
   }
 
   setBaseCurrency(event) {
     this.baseCurrency = event.target.value;
-    this.exchangeService.subscribe(this.baseCurrency, this.targetCurrency);
-    this.updatedValue();
+    if (this.targetCurrency !== undefined) {
+      this.Observer = new Observable(this.subscribe(this.baseCurrency, this.targetCurrency));
+      const action = (value) => {
+        this.exchangeRate = value['rates'][this.targetCurrency];
+        this.getResult();
+      };
+      const Observer = this.Observer;
+      const observable = Observer
+        .subscribe({ next: action });
+    }
+
   }
   setTargetCurrency(event) {
     this.targetCurrency = event.target.value;
-    this.exchangeService.subscribe(this.baseCurrency, this.targetCurrency);
-    this.updatedValue();
+    if (this.baseCurrency !== undefined) {
+      this.Observer = new Observable(this.subscribe(this.baseCurrency, this.targetCurrency));
+      const action = (value) => {
+        this.exchangeRate = value['rates'][this.targetCurrency];
+        this.getResult();
+      };
+      const Observer = this.Observer;
+      const observable = Observer
+        .subscribe({ next: action });
+    }
   }
 
-  updatedValue() {
-    return this.updatedCurrency = this.exchangeService.getUpdatedValue();
+  getResult() {
+    if (this.base.value !== null) {
+      this.updatedValue = this.exchangeRate * this.base.value;
+      return this.updatedValue;
+    }
+  }
+
+  subscribe(currency1, currency2) {
+    return (subscriber) => {
+      const url = `https://api.exchangeratesapi.io/latest?base=${currency1}&symbols=${currency2}`;
+
+      this.httpClient
+        .get(url).subscribe(value => {
+          subscriber.next(value);
+        });
+    }
   }
 }
