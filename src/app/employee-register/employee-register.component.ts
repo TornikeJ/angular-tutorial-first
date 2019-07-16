87002f556474f519d473197d1e6ed1bb3f3deb11
Tornike Janashia <t.janashia11@gmail.com>
2019-07-16 18:42:08 +0400

    feat(26-animation): Fix practice

--- src/app/employee-register/employee-register.component.ts
+++ src/app/employee-register/employee-register.component.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormControl } from '@angular/forms';
 import { EmployeesService } from '../employees.service';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
 
 @Component({
   selector: 'app-employee-register',
   templateUrl: './employee-register.component.html',
-  styleUrls: ['./employee-register.component.scss']
+  styleUrls: ['./employee-register.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        transform: 'translateX(0)'
+      })),
+      state('close', style({
+        display: 'none'
+      })),
+      transition('* => *', animate('0.2s')
+      )
+    ])
+  ]
 })
 export class EmployeeRegisterComponent implements OnInit {
   registerForm;
+  isInitilized = false;
   constructor(
     private formBuilder: FormBuilder,
     private employeeService: EmployeesService) { }
@@ -26,6 +46,7 @@ export class EmployeeRegisterComponent implements OnInit {
   onSubmit(form) {
     if (form.status === 'VALID') {
       this.employeeService.addEmployees(form.value).subscribe();
+      this.isInitilized = true;
       form.reset();
     } else {
       window.alert('Fill required fields');
